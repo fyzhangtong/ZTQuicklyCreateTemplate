89ea6fecc200dea34e4ad55d90607fc4203a18c9
fyzhangtong <243816365@qq.com>
2023-04-11 14:40:46 +0800

    添加测试代码

---
+++ ZTQuicklyCreateTemplate/Classes/ZTTest.h
@@ -0,0 +1,18 @@
+//
+//  ZTTest.h
+//  auson
+//
+//  Created by 张通 on 2023/4/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZTTest : NSObject
+
+- (void)logTest;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZTQuicklyCreateTemplate/Classes/ZTTest.m
@@ -0,0 +1,17 @@
+//
+//  ZTTest.m
+//  auson
+//
+//  Created by 张通 on 2023/4/11.
+//
+
+#import "ZTTest.h"
+
+@implementation ZTTest
+
+- (void)logTest
+{
+    NSLog(@"***********这是打印");
+}
+
+@end
